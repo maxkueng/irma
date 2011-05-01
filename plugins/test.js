34e6b84cbeee4d5f199b57dc4c9730e6304f7673
Max Kueng <me@maxkueng.com>
2011-05-01 18:09:28 +0200

    Removed test plugin

--- plugins/test.js
+++
@@ -1,5 +0,0 @@
-exports.init = function (y, config, messages, cron, logger) {
-	messages.add('test', 'This is a test');
-	messages.add('test', 'This is the biggest test yet');
-	messages.add('test', 'I\'m so testing this');
-};
