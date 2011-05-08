e482a8ba87b9b2ec8dbab76f902eaa1aa824356e
Max Kueng <me@maxkueng.com>
2011-05-09 01:11:39 +0200

    Kill plugin to exit process

---
+++ plugins/kill.js
@@ -0,0 +1,36 @@
+exports.init = function (y, config, messages, cron, logger) {
+	messages.add('kill_bye', "Alright, I'm off.");
+	messages.add('kill_bye', "Bye bye.");
+
+	messages.add('kill_permissiondenied', "You can't kill me! Only an admin can do that.");
+
+	var yammer_account = config.yammer[config.yammer_account];
+
+	y.on('message', function (message) {
+		if (/(kill|die|shut\s*down)/i.test(message.plainBody())) {
+			var thread = y.thread(message.threadId());
+			thread.setProperty('type', 'kill');
+
+			if (message.senderId() == yammer_account.admin_user_id) {
+				var killMessage = messages.get('kill_bye');
+
+				y.sendMessage(function (msg) {
+					logger.info('sending kill message: OK');
+					thread.setProperty('status', 'closed');
+					y.persistThread(thread);
+					logger.warn('exiting');
+					process.exit(0);
+				}, killMessage, { 'reply_to' : message.id() });
+
+			} else {
+				var permissiondeniedMessage = messages.get('kill_permissiondenied');
+				logger.warn('permission denied for kill command');
+
+				y.sendMessage(function (msg) {
+					thread.setProperty('status', 'closed');
+					y.persistThread(thread);
+				}, permissiondeniedMessage, { 'reply_to' : message.id() });
+			}
+		}
+	});
+};
