6d17976260a39d52be28dc1b814d38fcdaa3a304
Max Kueng <me@maxkueng.com>
2011-05-15 03:11:49 +0200

    Changed listen IP

--- plugins/webinterface.js
+++ plugins/webinterface.js
@@ -52,7 +52,6 @@ exports.init = function (y, config, messages, cron, logger) {
 	http.createServer(function (req, res) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end(getHTML());
-	}).listen(1337, "127.0.0.1");
-	console.log('Server running at http://127.0.0.1:1337/');
+	}).listen(1337, "0.0.0.0");
 
 };
