f298a91cba512f77017e4e924e098e0dec890210
Max Kueng <me@maxkueng.com>
2012-05-02 14:49:44 +0200

    Messages

--- plugins/spaghetti.js
+++ plugins/spaghetti.js
@@ -11,7 +11,6 @@ exports.init = function (y, config, messages, cron, logger) {
 
 	messages.add('spaghetti_threadclosed', "Sorry, you're to late.");
 	messages.add('spaghetti_threadclosed', "Too late!");
-	messages.add('spaghetti_threadclosed', "Forget it!");
 
 	var yammer_account = config.yammer[config.yammer_account];
 
