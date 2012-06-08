c3d219784986cf7048404fea6a2691b3a5a37394
Max Kueng <me@maxkueng.com>
2012-06-08 14:00:36 +0200

    Stock health message and rescheduled

--- plugins/spaghetti-stockwarning.js
+++ plugins/spaghetti-stockwarning.js
@@ -3,9 +3,9 @@ exports.init = function (y, config, messages, cron, logger) {
 	var Stock = stocks.Stock;
 
 	messages.add('spaghetti_stockwarning_critical', 'Warning! Spaghetti stock health is [health]. We have about [stock] grams left. Average consumption per week is [avg_consumption] grams. We can still feed [stock_rations] people with this. It might work out but there is a high probability that it won\'t.\nWho\'s going to restock? I\'m only a stupid robot and can\'t do it myself.');
-	messages.add('spaghetti_stockwarning_bad', 'Warning! Spaghetti stock health is [health]. We have about [stock] grams left. Average consumption per week is [avg_consumption] grams. We can still feed [stock_rations] people with this. It might work out but there is a high probability that it won\'t.\nWho\'s going to restock? I\'m only a stupid robot and can\'t do it myself.');
+	messages.add('spaghetti_stockwarning_bad', 'Warning! Spaghetti stock health is [health]. We have about [stock] grams left. Average consumption per week is [avg_consumption] grams. We can still feed [stock_rations] people with this. This will not be enough for another day of spaghetti.\nWho\'s going to restock? I\'m only a stupid robot and can\'t do it myself.');
 
-	new cron.CronJob('0 0 14 * * 4', function () {
+	new cron.CronJob('0 0 14 * * 3', function () {
 
 		var stock = stocks.get('03032ac58f81');
 		var stockInfo = stock.info();
