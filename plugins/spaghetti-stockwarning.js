c80e0ae0d2253b5be645c5935dc8a8d0e846ffb7
Max Kueng <me@maxkueng.com>
2012-07-07 19:32:40 +0200

    jslint beautification

--- plugins/spaghetti-stockwarning.js
+++ plugins/spaghetti-stockwarning.js
@@ -1,21 +1,25 @@
+"use strict";
+
 exports.init = function (y, config, messages, cron, logger) {
-	var stocks = require('./kiosk/stocks');
-	var Stock = stocks.Stock;
+	var stocks = require('./kiosk/stocks'),
+		Stock = stocks.Stock,
+		warningCron;
 
 	messages.add('spaghetti_stockwarning_critical', 'Warning! Spaghetti stock health is [health]. We have about [stock] grams left. Average consumption per week is [avg_consumption] grams. We can still feed [stock_rations] people with this. It might work out but there is a high probability that it won\'t.\nWho\'s going to restock? I\'m only a stupid robot and can\'t do it myself.');
 	messages.add('spaghetti_stockwarning_bad', 'Warning! Spaghetti stock health is [health]. We have about [stock] grams left. Average consumption per week is [avg_consumption] grams. We can still feed [stock_rations] people with this. This will not be enough for another day of spaghetti.\nWho\'s going to restock? I\'m only a stupid robot and can\'t do it myself.');
 
-	new cron.CronJob('0 0 14 * * 3', function () {
+	warningCron = new cron.CronJob('0 0 14 * * 3', function () {
+		var stock, stockInfo, stockMessage;
 
-		var stock = stocks.get('03032ac58f81');
-		var stockInfo = stock.info();
+		stock = stocks.get('03032ac58f81');
+		stockInfo = stock.info();
 
-		if (stockInfo.health == 'critical' || stockInfo.health == 'bad') {
+		if (stockInfo.health === 'critical' || stockInfo.health === 'bad') {
 
-			var stockMessage = messages.get('spaghetti_stockwarning_' + stockInfo.health, {
-				'health' : stockInfo.health, 
-				'stock' : stockInfo.stock, 
-				'avg_consumption' : stockInfo.avgUnits, 
+			stockMessage = messages.get('spaghetti_stockwarning_' + stockInfo.health, {
+				'health' : stockInfo.health,
+				'stock' : stockInfo.stock,
+				'avg_consumption' : stockInfo.avgUnits,
 				'stock_rations' : Math.round((stockInfo.stock / 150) * 10) / 10
 			});
 
@@ -28,6 +32,6 @@ exports.init = function (y, config, messages, cron, logger) {
 			}, stockMessage);
 
 		}
-			
+
 	});
 };
