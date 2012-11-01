7e2a6fe70bdc70abff95830ba80d6b1cae6389dc
Max Kueng <me@maxkueng.com>
2012-11-01 14:41:58 +0100

    Reboot

--- plugins/kill.js
+++ plugins/kill.js
@@ -1,40 +1,23 @@
 "use strict";
 
 exports.init = function (y, config, messages, cron, logger) {
-	messages.add('kill_bye', "Alright, I'm off.");
-	messages.add('kill_bye', "Bye bye.");
-
-	messages.add('kill_permissiondenied', "You can't kill me! Only an admin can do that.");
+	messages.add('kill_reboot_confirm', "I'll be back.");
 
 	y.on('message', function (message) {
-		var thread, sender, killMessage, permissiondeniedMessage;
+		if (/\b!reboot\b/i.test(message.plainBody())) {
+			logger.info("!reboot");
 
-		if (/\b(kill|die|shut\s*down)\b/i.test(message.plainBody())) {
-			thread = y.thread(message.threadId());
-			thread.setProperty('type', 'kill');
-
-			sender = y.user(message.senderId());
-
-			if (sender.isAdmin()) {
-				killMessage = messages.get('kill_bye');
-
-				y.sendMessage(function (msg) {
-					logger.info('sending kill message: OK');
-					thread.setProperty('status', 'closed');
-					y.persistThread(thread);
-					logger.warn('exiting');
-					process.exit(0);
-				}, killMessage, { 'reply_to' : message.id() });
-
-			} else {
-				permissiondeniedMessage = messages.get('kill_permissiondenied');
-				logger.warn('permission denied for kill command');
-
-				y.sendMessage(function (msg) {
-					thread.setProperty('status', 'closed');
-					y.persistThread(thread);
-				}, permissiondeniedMessage, { 'reply_to' : message.id() });
-			}
+			rebootConfirmMessage = messages.get('kill_reboot_confirm');
+				logger.info('sending reboto confirm message: OK');
+				var thread = y.thread(msg.threadId());
+				thread.setProperty('type', 'reboot');
+				thread.setProperty('status', 'open');
+				y.persistThread(thread);
+				logger.warn('exiting');
+				process.exit(0);
+			y.sendMessage(function (msg) {
+			}, rebootConfirmMessage, { 'reply_to' : message.id() });
 		}
 	});
+
 };
